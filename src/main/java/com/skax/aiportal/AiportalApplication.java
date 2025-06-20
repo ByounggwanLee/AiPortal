f6fe67a62299733c8ea9833e689526447ffa540c
이병관(LEE Byounggwan)/Architect/SWAT팀/SK <07584@skax.co.kr>
2025-06-20 10:29:25 +0900

    Java Project Create

---
+++ src/main/java/com/skax/aiportal/AiportalApplication.java
@@ -0,0 +1,13 @@
+package com.skax.aiportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skax/aiportal/AiportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skax.aiportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
